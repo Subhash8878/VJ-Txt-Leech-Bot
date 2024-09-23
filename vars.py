6e0bfe41f36b234bfd8c729a9c0e33700198c3e2
Subhash8878 <subhash.mahajan8878@gmail.com>
2024-09-23 07:27:45 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,6 +4,6 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
+API_ID = int(environ.get("API_ID", "23056713"))
+API_HASH = environ.get("API_HASH", "db3159297b3705b543b85937fd84e9a2")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
